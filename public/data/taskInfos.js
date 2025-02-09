89020a4ed60509318cba366b5b2277644bca84e0
PeirongAn <aprnl001@gmail.com>
2025-02-09 13:26:00 +0800

    fix: fix bug

--- public/data/taskInfos.js
+++ public/data/taskInfos.js
@@ -15,62 +15,62 @@ window.taskInfos = [
     descriptionEn: "Find me a rectangular, brown, wooden photo frame behind a flower, under a model house and above a tray with lots of balls in the living room. And the living room is on the right side of a small drawing room and next to a garden."
   },
   {
-    id: '4',
+    id: '3',
     descriptionZh: '那是一个中国风格的陶瓷花瓶，里面种了一株叶子翠绿的木本植物。它很容易被发现，放在一张铺着红色桌布的矮茶几上，位于两本杂志的中间。这栋建筑的一楼有一个餐厅、一条长长的走廊和许多通往不同房间的门。',
     descriptionEn: "That's a ceramic vase in Chinese style and planted a woody plant with emerald leaves. It can be easily found on a red tablecloth of a short end table and in the middle of two magazines. There's a dining hall, a long hallway and many doors on the ground floor of the building leading to different rooms."
   },
   {
-    id: '5',
+    id: '4',
     descriptionZh: '请帮我找一扇玻璃制的、高大的、长方形的门，它安装在浴室的角落里。浴室位于一楼，靠近一个厕所。这扇门靠近一个银色的水槽和一个棕色的衣柜。',
     descriptionEn: "Please help me find a glass, tall, rectangular door which is set in the corner of a bathroom. The bathroom is located on first floor and near a toilet. The door is near a silvery sink and a brown wardrobe."
   },
   {
-    id: '6',
+    id: '5',
     descriptionZh: '我想找一张布质的、大的、白色的床，它位于一间卧室里。卧室位于二楼，靠近一个洗手间。床上有两个蓝色的枕头和一块黄色的织物，旁边还有两张沙发。',
     descriptionEn: "I want to find a cloth, big, white bed which is located in a bedroom. The bedroom is located on second floor and near a lavatory. There are two blue pillows and a yellow fabric on the bed and two sofas near it."
   },
   {
-    id: '7',
+    id: '6',
     descriptionZh: '请帮我找一个银色的、坚固的、立方体的冰箱，它位于餐厅里。冰箱附近有一个橱柜和一个台面。餐厅通过一条走廊与客厅相连。',
     descriptionEn: "Please help me find a silvery, solid, cubic refrigerator which is located in a dining room. There is a cabinet and a countertop near the refrigerator. The dining room is connected with a living room by a corridor."
   },
   {
-    id: '8',
+    id: '7',
     descriptionZh: '帮我找一把深褐色的凳子，它有高高的靠背和木质的凳腿，放在一张黄色地毯上，下方有两盏吊灯。它正对着一个镜子，镜子位于一个橱柜上方，处于长方形桌子的长边一侧，靠近桌子的角落。不过，这是更靠近墙上素描画的那一把。',
     descriptionEn: "Help me to find a fuscous stool with a tall backrest and wooden stool legs on a yellow carpet below two chandeliers. It faces a mirror above a cupboard, at the long edge of the rectangular table and next to the corner of the table. But, that's the one which is closer to a sketch picture on the wall."
   },
   {
-    id: '9',
+    id: '8',
     descriptionZh: '在客厅里有一个木制雕塑，形状像一只有翅膀的猪，客厅有高高的天花板和三盏吊灯。雕塑位于一张高大的木桌中央，旁边是一个炉子，后面是一张圆形的茶几。此外，它正对着一扇滑动门。',
     descriptionEn: "There's a wooden sculpture shaped like a pig with wings in the living room with high ceilings and three chandliers. The sculpture is in the center of a tall and wooden table, beside a stove and behind a round end table. In addition, It faces a sliding door."
   },
   {
-    id: '10',
+    id: '9',
     descriptionZh: '在二楼的卧室里找一张灰色的、棉质的、长方形的沙发。它位于一个棕色的小柜子和一张灰色的椅子之间。卧室紧邻楼梯。',
     descriptionEn: "Find a grey, cotton and rectangular sofa in the bedroom in the second floor. It is between a small brown cabinet and a grey chair. The bedroom is next to a stair."
   },
   {
-    id: '11',
+    id: '10',
     descriptionZh: '我想找一个高大的、白色的、木质的床头柜。床头柜上放着一束白色的花，旁边是一张白色的床。床头柜放在一间位于二楼的卧室里。这间卧室位于另一间卧室和浴室之间。',
     descriptionEn: "I want to find a tall, white, woody nightstand. There is a bunch of white flower on the nightstand and a white bed near it. The nightstand is set in a bedroom which is set on second floor. The bedroom is set in the middle of a bedroom and a bathroom."
   },
   {
-    id: '12',
+    id: '11',
     descriptionZh: '在浴室的角落里，有一个白色花瓶里种着绿白相间的植物，叶子很长，位于浴缸的左侧，窗户前面，镜子下方。浴室与一间卧室和小客厅共用一条走廊。',
     descriptionEn: "At the corner of the washroom, there is a green and white plant in a white vase and with long leaves on the left of the bathtub, in front of a window and under a mirror. The washroom shares a corridor with a bedroom and the small living room."
   },
   {
-    id: '13',
+    id: '12',
     descriptionZh: '在厕所里，有一个罐子放在镜子下方、桌子上，位于一个橙色瓶子的左侧。厕所位于小客厅和卧室之间，正对着墙上的一幅画。此外，这个罐子是玻璃制的，透明的，里面装着一些肥皂。',
     descriptionEn: "There is a jar under a mirror, on the table and on the left of an orange bottle in a toilet. The toilet is between the small living room and a bedroom and opposite to a picture on the wall. What's more, the jar is glass, limpid and contains some soaps."
   },
   {
-    id: '14',
+    id: '13',
     descriptionZh: '我想找一个银色的、透明的、长方形的洗手池。洗手池下面有一个柜子，前面有一张沙发。洗手池位于花园的角落，靠近草地。这个角落与餐厅相连。',
     descriptionEn: "I want to find a silvery, transparent, rectangular sink. There is a cabinet under the sink and a sofa in front of it. The sink is set at the corner of the garden which is near the grass. The corner is connected with a dining room."
   },
   {
-    id: '15',
+    id: '14',
     descriptionZh: '在一楼的客厅里找一个鸟状的、小巧的、色彩斑斓的标本。它靠近一扇窗户和一张黄色的桌子。客厅里有一个炉子，并且靠近一张台球桌。',
     descriptionEn: "Find a bird-liked, small and colorful specimen in the living room in the first floor. It is near a window and near a yellow desk. The living room has a stove and near a billiards table."
   }
