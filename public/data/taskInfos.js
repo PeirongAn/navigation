9da6d870ba9a14ad6a3a36b67a03cfc77a379398
anpeirong <aprnl001@gmail.com>
2025-02-12 15:08:15 +0800

    fix: fix bug

--- public/data/taskInfos.js
+++ public/data/taskInfos.js
@@ -10,54 +10,49 @@ window.taskInfos = [
     descriptionZh: "请找到一个有四个搁板的大型木质柜子。你首先会到达一个圆形大厅，那里有一扇通向方形储物室的开门。你会看到里面有一块红地毯，柜子在右侧，靠近开关，上面陈列着六张CD。",
   },
   {
-    id: "2",
+    id: "3",
     descriptionEn: "Please help me find a long, soft, yellow fabric which is located in a living room.The living room is connected with the garden and a dining room. The fabric is set on a sofa and near a TV.",
     descriptionZh: "请帮我找到位于客厅里的一块又长又软的黄色织物。这个客厅与花园和餐厅相连。这块织物放在沙发上，靠近电视。",
   },
   {
-    id: "3",
+    id: "4",
     descriptionEn: "Please find a big, silvery, thin floor lamp which is located in the corner of a living room. The floor lamp is surrounded by two grey sofa and near a brown carpet. The living room is connected with the garden and a dining room.",
     descriptionZh: "请找到位于客厅角落里的一盏大型、银色、纤细的落地灯。这盏落地灯周围有两张灰色沙发，附近有一块棕色地毯。这个客厅与花园和餐厅相连。",
   },
   {
-    id: "4",
+    id: "5",
     descriptionEn: "I want to find a black and white, soft and square pillow. On the left of a black pillow on the sofa, the pillow is on the left of a short table and opposite to the display in the vast parlour. It's near the garden, on the right side of a small drawing room.",
     descriptionZh: "我想找一个黑白相间、柔软的方形抱枕。它在沙发上的一个黑色抱枕的左边，位于一张矮桌的左侧，在宽敞客厅里的展示柜对面。它靠近花园，在一个小起居室的右侧。",
   },
   {
-    id: "5",
+    id: "6",
     descriptionEn: "Please help me find a rectangular ,large, blue window which is set on the wall of a dining room. There is a sink and countertop opposite to the window. The dining room is connected with a living room by a corridor.",
     descriptionZh: "请帮我找到安装在餐厅墙上的一扇矩形、大型、蓝色的窗户。窗户对面有一个水槽和操作台。餐厅通过走廊与客厅相连。",
   },
   {
-    id: "6",
+    id: "7",
     descriptionEn: "Find a small, rectangular and small window in the kitchen. It is near a sink and near a long table. The kitchen near a stair.",
     descriptionZh: "在厨房里找到一扇小型的矩形窗户。它靠近水槽和一张长桌。这个厨房靠近楼梯。",
   },
   {
-    id: "7",
+    id: "8",
     descriptionEn: "Please find a white and ceramic closestool beside a small cabin and below a white picture and a black towel rack on the wall. You can find it in a compartment of the bathroom and the shower cubicle of the restroom on the first floor. In addition, it's faced with a window and another towel rack.",
     descriptionZh: "请找到一个白色陶瓷马桶，它在一个小隔间旁边，墙上的一幅白色画作和黑色毛巾架的下方。你可以在一楼浴室的隔间和淋浴间里找到它。此外，它面对着一扇窗户和另一个毛巾架。",
   },
   {
-    id: "8",
+    id: "9",
     descriptionEn: "Go to find a sofa with a super high backrest and golden coral patterns close to a black table. It's behind the sliding door in the living room in the first floor. That's a room with a high ceiling and three chandeliers.",
     descriptionZh: "去找一张靠背超高、带有金色珊瑚图案的沙发，它靠近一张黑色桌子。它在一楼客厅的推拉门后面。那是一个有着高天花板和三盏吊灯的房间。",
   },
   {
-    id: "9",
+    id: "10",
     descriptionEn: "Could you help me find the pool table with red surface and two cue put on it right below a long ceiling lamp? It's in the middle of two sliding door and can be easily seen in the center of the tile floor in the lobby. There is a flower bed and a fountain outside, in the courtyard.",
     descriptionZh: "你能帮我找到那张台面是红色的台球桌吗？上面放着两根球杆，正好在一盏长吊灯下方。它位于两扇推拉门中间，可以在大厅瓷砖地板的中央轻易看到。外面的庭院里有一个花坛和一个喷泉。",
   },
   {
-    id: "10",
+    id: "11",
     descriptionEn: "Please help me find a big, white, soft bed which is located in the centre of a bedroom which is located on second floor. There is a small black clock on the nightstand near the bed and a soft black fabric on the bed. The bedroom is set in the middle of a bedroom and a bathroom.",
     descriptionZh: "请帮我找到一张位于二楼卧室中央的大型、白色、柔软的床。床边床头柜上有一个小黑钟，床上有一块柔软的黑色织物。这个卧室位于另一间卧室和浴室之间。",
-  },
-  {
-    id: "11",
-    descriptionEn: "I want to find a silvery, metal, curved sink. The sink is set near a washing machine and a under a cabinet. The sink is located in the corner of a bathroom. The bathroom is located on first floor and near a toilet.",
-    descriptionZh: "我想找一个银色的、金属的、弧形的水槽。这个水槽在洗衣机旁边，一个柜子下方。水槽位于浴室的角落里。这个浴室在一楼，靠近厕所。",
   },
   {
     id: "12",
@@ -66,36 +61,41 @@ window.taskInfos = [
   },
   {
     id: "13",
+    descriptionEn: "I want to find a silvery, metal, curved sink. The sink is set near a washing machine and a under a cabinet. The sink is located in the corner of a bathroom. The bathroom is located on first floor and near a toilet.",
+    descriptionZh: "我想找一个银色的、金属的、弧形的水槽。这个水槽在洗衣机旁边，一个柜子下方。水槽位于浴室的角落里。这个浴室在一楼，靠近厕所。",
+  },
+  {
+    id: "14",
     descriptionEn: "Find a yellow, wooden and tall cabinet in the kitchen in the first floor. It is nears a sink and faces a long table. The kitchen next to a stair.",
     descriptionZh: "在一楼的厨房里找到一个黄色的、木质的高柜。它靠近水槽，面对着一张长桌。这个厨房在楼梯旁边。",
   },
   {
-    id: "14",
+    id: "15",
     descriptionEn: "Find me a black, cuboid and limpid box, which is in front of a mirror, on the left of another shorter box and on the right side of a pillar.It's set in the corridor, and the corridor is leading to the gate and on the left of a small drawing room.",
     descriptionZh: "帮我找一个黑色的、长方体的透明盒子，它在镜子前面，在另一个较矮的盒子的左边，在一根柱子的右侧。它放在走廊里，这个走廊通向大门，位于一个小起居室的左边。",
   },
   {
-    id: "15",
+    id: "16",
     descriptionEn: "I want to find a blooming, big, white flower which is located in a dining room. There is a table surrounded by black chairs under the flower and a clock near it. The dining room is connected with a living room by a corridor.",
     descriptionZh: "我想找到一朵盛开的、大型的白色花，它位于餐厅里。花下面有一张桌子，周围环绕着黑色椅子，附近有一个时钟。这个餐厅通过走廊与客厅相连。",
   },
   {
-    id: "16",
+    id: "17",
     descriptionEn: "Find a thin, small and metal candlestick in the kitchen in the first floor. It is between a taller candlestick and ceramic kettle in the right. The kitchen is near a stair.",
     descriptionZh: "在一楼的厨房里找到一个纤细的、小型的金属烛台。它在一个较高的烛台和右边的陶瓷水壶之间。这个厨房靠近楼梯。",
   },
   {
-    id: "17",
+    id: "18",
     descriptionEn: "Please help me find a silvery, solid, cubic refrigerator which is located in a dining room. There is a cabinet and a countertop near the refrigerator. The dining room is connected with a living room by a corridor .",
     descriptionZh: "请帮我找到位于餐厅里的一个银色的、结实的、立方体的冰箱。冰箱附近有一个柜子和操作台。这个餐厅通过走廊与客厅相连。",
   },
   {
-    id: "18",
+    id: "19",
     descriptionEn: "Find a small, round and black helmet in the living room. It is above a cabinet and there is a pair of white shoes in the right. The living room is in the second floor.",
     descriptionZh: "在客厅里找到一个小型的、圆形的黑色头盔。它在一个柜子上面，右边有一双白色鞋子。这个客厅在二楼。",
   },
   {
-    id: "19",
+    id: "20",
     descriptionEn: "Find a rectangular, cotton and cream color sofa in the living room. There is the same sofa on the left of it. The living room has a large celling lamp.",
     descriptionZh: "在客厅里找到一张矩形的、棉质的米色沙发。它的左边有一张相同的沙发。这个客厅有一盏大型吊灯。",
   }
